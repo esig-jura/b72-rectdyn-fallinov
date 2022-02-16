d17ce8878d90508d1d226f64416a929dbe240b40
SFA <steve.fallet@divtec.ch>
2022-02-16 11:40:01 +0100

    Envoi formulaire et récup haut et lar

--- script.js
+++ script.js
@@ -9,6 +9,20 @@
 (function main() {
     'use strict'; // Demande un interprétation stricte du code
 
-    console.log('Votre code ici !');
+    const formulaire = document.getElementById('formRect');
+    const rectangle = document.getElementById('rectangle');
+    const hauteur = document.getElementById('hauteur');
+    const largeur = document.getElementById('largeur');
+
+    console.log(formulaire, hauteur, largeur, rectangle);
+
+
+    formulaire.addEventListener('submit', function miseAJour(event) {
+        event.preventDefault(); // Stop l'envoi du formulaire
+        rectangle.style.height = hauteur.value + 'px';
+        rectangle.style.width = largeur.value + 'px';
+    });
+
+
 
 }()); // Main IIFE
